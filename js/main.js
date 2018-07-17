697bc070201456f6a1dfee106b590d6f2e708778
YUMEE <yumeehk@hotmail.com>
2018-07-17 16:05:45 -0400

    Error class gets added after initial submit button click.

--- js/main.js
+++ js/main.js
@@ -15,8 +15,18 @@ var closeButton = document.querySelector('.close');
 
 closeButton.addEventListener('click', function() {
   form.style.display = 'none';
-})
+});
 
+// error on user and pass when click on submit button
+
+var user = document.getElementsByName("user")[0];
+var pass = document.getElementsByName("pass")[0];
+var submitButton = document.querySelector('.submit');
+
+submitButton.addEventListener('click', function() {
+  user.classList.add('error');
+  pass.classList.add('error');
+});
 
 
 });
