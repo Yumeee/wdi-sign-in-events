582ef353b6992d877691ad337f5f65dc75d71785
YUMEE <yumeehk@hotmail.com>
2018-07-17 17:03:13 -0400

    Edit in fields using a for loop for removing .error and make .stopPropagation

--- js/main.js
+++ js/main.js
@@ -13,8 +13,9 @@ signIn.addEventListener('click', function() {
 // closing form
 var closeButton = document.querySelector('.close');
 
-closeButton.addEventListener('click', function() {
+closeButton.addEventListener('click', function(event) {
   form.style.display = 'none';
+  event.stopPropagation();
 });
 
 // error on user and pass when click on submit button
@@ -23,10 +24,29 @@ var user = document.getElementsByName("user")[0];
 var pass = document.getElementsByName("pass")[0];
 var submitButton = document.querySelector('.submit');
 
-submitButton.addEventListener('click', function() {
+submitButton.addEventListener('click', function(event) {
   user.classList.add('error');
   pass.classList.add('error');
+  event.stopPropagation();
 });
+
+
+// don't let fields to be clicked to close form and remove error
+var fields = document.querySelectorAll('.field');
+
+fields.forEach(function(field) {
+  field.addEventListener('mouseover', function() {
+    field.classList.remove('error');
+  });
+  field.addEventListener('click', function(event) {
+    event.stopPropagation()
+  });
+});
+
+// closing modal when clicking on it
+form.addEventListener('click', function() {
+  form.style.display = 'none';
+})
 
 
 });
